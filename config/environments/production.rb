93949e0f9f2ccb20505c55f684b84580e1ea3618
stephanesdr <stadlerstephane@gmail.com>
2019-06-27 20:34:10 +0200

    Add pre set up for ENV variable of mailjet

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    config.action_mailer.delivery_method = :mailjet
+    config.action_mailer.default_url_options = { host: ENV['NOM_DU_PROJET'] }
 end
